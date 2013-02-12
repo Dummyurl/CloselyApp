a30f3d34ff07def4d34be47b1f74deab79a9af58
Your Name <shoppix@ubuntu.(none)>
2013-02-12 23:49:40 +0200

    navigation bubble

--- application/views/side/buttons.php
+++ application/views/side/buttons.php
@@ -1,4 +1,9 @@
-﻿		        	<div id="headline">								
+		        	<div id="headline">	
+						<div class="navigation-bubble">
+							<div class="bubble-top"></div>
+							<div class="bubble-center"></div>	
+							<div class="bubble-bottom"></div>	
+						</div>					
 						<div class="mainbutton">
 							<span class="left"><a href="#" class="button" id="b1"><img src="<?php echo base_url();?>asset/img/buttons/1.png" alt="alt text" /><p>פתח מכרז חדש</p></a></span>
 														<span class="left"><a href="#" class="button" id="b2"><img src="<?php echo base_url();?>asset/img/buttons/6.png" alt="alt text" /><p>קבל הצעת מחיר</p></a></span>
@@ -12,4 +17,5 @@
 													<span class="left"><a href="#" class="button" id="b6"><img src="<?php echo base_url();?>asset/img/buttons/4.png" alt="alt text" /><p>הזמן שרות</p></a></span>
 						</div>
 		        		<em id="corner"></em>
+						
 		        	</div>
