c9ebbf67e4bf45f9926fb194efb309c569faad0b
Your Name <shoppix@ubuntu.(none)>
2013-02-28 00:01:41 +0200

    button icons

--- application/views/side/buttons.php
+++ application/views/side/buttons.php
@@ -9,14 +9,14 @@
 						</div>					
 						<div class="mainbutton">
 							<span class="left"><a href="#" class="button" id="b1"><img src="<?php echo base_url();?>asset/img/buttons/1.png" alt="alt text" /><p>אופנה וביגוד</p></a></span>
-														<span class="left"><a href="#" class="button" id="b2"><img src="<?php echo base_url();?>asset/img/buttons/6.png" alt="alt text" /><p>אלקטרוניקה וחשמל</p></a></span>
+														<span class="left"><a href="#" class="button" id="b2"><img src="<?php echo base_url();?>asset/img/buttons/6.png" alt="alt text" /><p>'חשמל ואלק</p></a></span>
 						</div>
 						<div class="mainbutton">
-														<span class="left"><a href="#" class="button" id="b3"><img src="<?php echo base_url();?>asset/img/buttons/2.png" alt="alt text" /><p>בילויים</p></a></span>
+														<span class="left"><a href="#" class="button" id="b3"><img src="<?php echo base_url();?>asset/img/buttons/2.png" alt="alt text" /><p>מסעדות ואוכל</p></a></span>
 														<span class="left"><a href="#" class="button" id="b4"> <img src="<?php echo base_url();?>asset/img/buttons/5.png" alt="alt text" /><p>יופי וקוסמטיקה</p></a></span>
 						</div>
 						<div class="mainbutton">
-														<span class="left"><a href="#" class="button" id="b5"><img src="<?php echo base_url();?>asset/img/buttons/3.png" alt="alt text" /><p>לבית ולגן</p></a></span>
+														<span class="left"><a href="#" class="button" id="b5"><img src="<?php echo base_url();?>asset/img/buttons/3.png" alt="alt text" /><p>בילויים ופנאי</p></a></span>
 													<span class="left"><a href="#" class="button" id="b6"><img src="<?php echo base_url();?>asset/img/buttons/4.png" alt="alt text" /><p>רכב ותחבורה</p></a></span>
 						</div>
 		        		<em id="corner"></em>
