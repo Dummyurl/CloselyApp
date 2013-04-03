8b6449e76504fabead640de1ed93abe166d8221c
Niso <nmazuz@gator904.hostgator.com>
2013-04-03 13:02:43 -0500

    css fixes

--- application/views/side/lastactions.php
+++ application/views/side/lastactions.php
@@ -1,7 +1,8 @@
+<?php $this->load->helper('url'); ?>
 <li class="box-row">
 	<div class="line-contant">
-		<img src="https://graph.facebook.com/<?php echo $lasttype['user_id'] ?>/picture"/>
-		<div class="feed_text"><?php echo $text . ' ' . $lasttype['feed'] . ' ב' . $store . ' ' .  $uname  ?></div>
+		<a href="<?php echo base_url();?>user/popup/<?php echo $lasttype['user_id'] ?>" class="fancybox"><img src="https://graph.facebook.com/<?php echo $lasttype['user_id'] ?>/picture"/></a>
+		<div class="feed_text"><?php echo $text . ' ' . $lasttype['feed'] . ' ב' . $store . ' <span class="addbold">' .  $uname . '</span>'  ?></div>
 		<div class="feed_text title"><?php echo $lasttype['title'] ?></div>
 	</div>
 </li>
